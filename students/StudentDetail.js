3f89b00375cef81d496f37e9420f938a05356149
sreeram315 <sreerammaram2@gmail.com>
2019-01-05 16:06:07 +0530

    Delete button

--- students/StudentDetail.js
+++ students/StudentDetail.js
@@ -5,6 +5,7 @@ class StudentDetail extends Component {
     super(props)
     this.NameClick = this.NameClick.bind(this)
     this.showDataToggle = this.showDataToggle.bind(this)
+    this.deleteStudentButtonHandle = this.deleteStudentButtonHandle.bind(this)
     this.state = {
       showDetails: true
     }
@@ -12,22 +13,33 @@ class StudentDetail extends Component {
   NameClick (event) {
     event.preventDefault()
     const { dataCallBack } = this.props
-    dataCallBack('hello re!')
+    let newStudent = this.props.student
+    newStudent['name'] = 'Gurukanth Manant'
+    if (dataCallBack !== undefined) { dataCallBack(newStudent) }
   }
+
+  deleteStudentButtonHandle (event) {
+    event.preventDefault()
+    const { deleteStudentCallBack } = this.props
+    let student = this.props.student
+    if (deleteStudentCallBack !== undefined) { deleteStudentCallBack(student) }
+  }
+
   showDataToggle (event) {
     event.preventDefault()
     const { showDetails } = this.state
     this.setState({
-      showDetails: !this.state.showDetails
+      showDetails: !showDetails
     })
   }
+
   render () {
     const { showDetails } = this.state
     const { student } = this.props
     return (
       <div>
         <h1 onClick={this.NameClick}> {student.name} </h1>
-        { showDetails == true
+        { showDetails === true
           ? <div>
             <h4> {student.reg_no} </h4>
             <h4>  CGPA: {student.cgpa} </h4>
@@ -37,6 +49,7 @@ class StudentDetail extends Component {
           </div>
           : '' }
         <button onClick={this.showDataToggle}>Show/hide Data</button>
+        <button onClick={this.deleteStudentButtonHandle}>Remove student</button>
       </div>
 
     )
--- students/StudentList.js
+++ students/StudentList.js
@@ -7,20 +7,51 @@ class StudentList extends Component {
   constructor (props) {
     super(props)
     this.handleDataCallBack = this.handleDataCallBack.bind(this)
+    this.handleDeleteStudentCallBack = this.handleDeleteStudentCallBack.bind(this)
+    this.state = {
+      StudentsDisplayed: null
+    }
   }
-  handleDataCallBack (text_message) {
-    alert(text_message)
+  handleDataCallBack (newStudent) {
+    let currentStudents = this.state.StudentsDisplayed
+    // currentStudents.push(newStudent)
+    let newStudents = currentStudents
+    this.setState({
+      StudentsDisplayed: newStudents
+    })
+  }
+  handleDeleteStudentCallBack (student) {
+    let currentStudents = this.state.StudentsDisplayed
+
+    var index = currentStudents.indexOf(student) // find the index and remove that student
+    if (index > -1) { currentStudents.splice(index, 1) }
+
+    let newStudents = currentStudents
+    this.setState({
+      StudentsDisplayed: newStudents
+    })
+  }
+  componentDidMount () {
+    this.setState({
+      StudentsDisplayed: StudentData
+    })
   }
   render () {
+    let { StudentsDisplayed } = this.state
     return (
-      <div onClick={this.titleClicked}>
-        <h1> Student List:  </h1>
-        {StudentData.map((item, index) => {
-          return <StudentDetail
-            student={item}
-            key={`post-list-key ${index}`}
-            dataCallBack={this.handleDataCallBack} />
-        })}
+      <div>
+        { StudentsDisplayed !== null && StudentsDisplayed.length !== 0
+          ? <div onClick={this.titleClicked}>
+            <h1> Student List:  </h1>
+            {StudentsDisplayed.map((item, index) => {
+              return <StudentDetail
+                student={item}
+                key={`post-list-key ${index}`}
+                dataCallBack={this.handleDataCallBack}
+                deleteStudentCallBack={this.handleDeleteStudentCallBack} />
+            })}
+          </div>
+          : <h1> No students to display </h1>}
       </div>
     )
   }
