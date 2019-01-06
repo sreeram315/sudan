14d126f48276faea28d0b8212d25b574b42a2c63
sreeram315 <sreerammaram2@gmail.com>
2019-01-06 12:58:27 +0530

    Form focus, submit

--- students/StudentForm.js
+++ students/StudentForm.js
@@ -3,17 +3,40 @@ import React, { Component } from 'react'
 class StudentForm extends Component {
   constructor (props) {
     super(props)
-    this.handleNameChange = this.handleNameChange.bind(this)
     this.state = {
       fullName: ''
     }
+    this.inputFullNameRef = React.createRef()
   }
 
-  handleNameChange (event) {
+  handleNameChange = (event) => {
     event.preventDefault()
     this.setState({
       [event.target.name]: event.target.value
     })
+  }
+
+  clearNameField = (event) => { 
+    event.preventDefault()
+    this.setState({
+      fullName: ''
+    })
+    this.inputFullNameRef.current.value = ''
+  }
+
+
+  focusOnName = (event) => {
+    event.preventDefault()
+    this.inputFullNameRef.current.focus()
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state)
+  }
+
+  componentDidMount () {
+    // this.inputFullNameRef.current.focus()
   }
 
   render () {
@@ -21,9 +44,10 @@ class StudentForm extends Component {
     return (
       <div>
         <h1>Full name is: {fullName}</h1>
-        <form method='POST'>
-          <p><input type='text' placeholder='Full Name' name='fullName' onChange={this.handleNameChange} /></p>
-
+        <form method='POST' onSubmit={this.handleFormSubmit}>
+          <p><input ref={this.inputFullNameRef} type='text' placeholder='Full Name' name='fullName' onChange={this.handleNameChange} /></p>
+          <button onClick={this.focusOnName}>Focus on Name</button>
+          <button onClick={this.clearNameField}>Clear name field</button>
           <button>Save</button>
         </form>
       </div>
